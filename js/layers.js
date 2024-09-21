c1148baf1441647bca563fa979967fd2b2abcbb6
tenten <159144181+INVADERover@users.noreply.github.com>
2024-09-21 22:21:23 +0900

    k

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
 addLayer("p", {
-    name: "training", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "Tr", // This appears on the layer's node. Default is the id with the first letter capitalized
+    name: "TRA", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "TRA", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -8,19 +8,20 @@ addLayer("p", {
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "Tra", // Name of prestige currency
+    resource: "Training", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
-        if (hasUpgrade('p', 15)) mult = mult.times(upgradeEffect('p', 15))
+        if (hasUpgrade('l', 12)) mult = mult.times(upgradeEffect('l', 12))
         if (hasUpgrade('p', 24)) mult = mult.times(upgradeEffect('p', 24))
         return mult
     },
-    gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
+    gainExp() { // Calculate the exponent on main currency from bonus
+        mult = new Decimal(1)
+        return mult
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
@@ -29,71 +30,48 @@ addLayer("p", {
     layerShown(){return true},
     
     upgrades: {
-        11: {title: "-11-<br>forgotten item",
-            description: "+1 base Exp gain",
-            cost: new Decimal(9),
-            unlocked(){return hasUpgrade("p",13)}},
-
-        12: {title: "-12-<br>Starting training",
-            description: "x1.5 Exp gain",
-            cost: new Decimal(2),},
-
-        13: {title: "-13-<br>Running",
-            description: "x2 Exp gain",
+        21: {
+            title: "-11-",
+            description: "x1.5 EXP gain",
             cost: new Decimal(2),
-            unlocked(){return hasUpgrade("p",12)}},
+            effect() {return 1.5},
+            unlocked(){
+            if ((player.a.points.gte(1))){return true}}
+            },
 
-        14: {title: "-14-<br>Run infinitely",
-            description: "Unlock the way to running infinitely",
-            cost: new Decimal(4),
-            unlocked(){return hasUpgrade("p",13)}},
-            
-        15: {title: "-15-<br>Streamlining training",
-            description: "x2 Tra gain",
-            cost: new Decimal(24),
-            effect() {return 2},
-            unlocked(){return hasUpgrade("p",14)}},
-
-
-        21: {title: "-21-<br>Visualization training 1",
-            description: "Exp boost Exp gain, but it is very weak.",
-            cost: new Decimal(1),
+        22: {
+            title: "-12-",
+            description: "EXP boost to EXP gain",
+            cost: new Decimal(2),
             effect() {return player.points.add(1).pow(0.04)},
             effectDisplay() {return format(this.effect())+"x"},
-            unlocked(){return hasUpgrade("p",12)}},
+            unlocked(){return hasUpgrade("p",21)}
+            },
 
-        22: {title: "-22-<br>Visualization training 2",
-            description: "Tra boost Exp gain, but it is very weak.",
-            cost: new Decimal(4),
+        23: {
+            title: "-13-",
+            description: "TRA boost to EXP gain",
+            cost: new Decimal(3),
+            effect() {return player[this.layer].points.add(1).pow(0.1)},
+            effectDisplay() {return format(this.effect())+"x"},
+            unlocked(){return hasUpgrade("p",22)}
+            },
+            
+        24: {title: "-14-",
+            description: "TRA boost to TRA gain",
+            cost: new Decimal(24),
             effect() {return player[this.layer].points.add(1).pow(0.08)},
             effectDisplay() {return format(this.effect())+"x"},
-            unlocked(){return hasUpgrade("p",21)}},
-
-        23: {title: "-24-<br>Motivation 1",
-            description: "Increase the amount of Exp gained based on AP",
-            cost: new Decimal(100),
-            effect() {return Decimal.pow(1.5, player.a.points)},
-            effectDisplay() {return format(this.effect())+"x"},
             unlocked(){
-                if ((player.l.points.gte(1)) && (hasUpgrade("p",22))){
-                    return true}}},
-
-        24: {title: "-24-<br>Motivation 2",
-            description: "Increase the amount of Tre gained based on AP",
-            cost: new Decimal(200),
-            effect() {return player.a.points},
-            effectDisplay() {return format(this.effect())+"x"},
-            unlocked(){
-                if ((player.l.points.gte(1)) && (hasUpgrade("p",22))){
-                    return true}}},
-            
+                if ((player.l.points.gte(1)) && (hasUpgrade("p",23))){return true}},
+            },            
     },
 
     buyables: {
         11: {
             title: "Running*n", // Optional, displayed at the top in a larger font
             cost(x) { // cost for buying xth buyable, can be an object if there are multiple currencies
-                return  Decimal.pow(10, x)
+                return  Decimal.pow(11, x)
             },
             effect(x) { // Effects of owning x of the items, x is a decimal
                 return Decimal.pow(2, x);
@@ -105,7 +83,7 @@ addLayer("p", {
                 Amount: " + player[this.layer].buyables[this.id] + "\n\
                 Currently: x" + format(data.effect)
             },
-            unlocked() { return hasUpgrade("p",14)},
+            unlocked() { return hasUpgrade("l",11)},
             canAfford() {
                 return player[this.layer].points.gte(tmp[this.layer].buyables[this.id].cost)},
             buy() {
@@ -133,24 +111,34 @@ addLayer("l", {
         points: new Decimal(0),
         level_cost: new Decimal(0),
     }},
-    color: "yellow",
-    resource: "Level", 
+    color: "green",
+    resource: "LVL", 
     row: "side",
 
     upgrades: {
-        11: {title: "Lvl.1",
-            description: "+1 base Exp gain",
-            canAfford(){return player.points.gte(tmp[this.layer].upgrades[this.id].cost)},
-            pay(){player.point = player.points.sub(this.cost())},
+        11: {
+            title: "Lvl.1",
+            description: "Unlock a new buyable to TRA",
+            currencyDisplayName:"EXP",
+            currencyInternalName:"points",
+            cost: new Decimal(50),
+            onPurchase(){player[this.layer].points = player[this.layer].points.add(1)},
+            },
+
+        12: {
+            title: "Lvl.2",
+            description: "LVL boost to TRA gain",
+            currencyDisplayName:"EXP",
+            currencyInternalName:"points",
+            cost: new Decimal(300),
+            effect() {return Decimal.pow(1.2,player.l.points,)},
+            effectDisplay() {return format(this.effect())+"x"},
+            onPurchase(){player[this.layer].points = player[this.layer].points.add(1)},
             }
         },
 
-    layerShown() { return hasUpgrade("p",15)},
+    layerShown() { return hasAchievement("a", 12)},
     milestonePopups: false,
-
-    
-    
-
 },)
 
 addLayer("a", {
@@ -159,7 +147,7 @@ addLayer("a", {
         points: new Decimal(0),
     }},
     color: "yellow",
-    resource: "achievement power", 
+    resource: "ACP", 
     row: "side",
     tooltip() { // Optional, tooltip displays when the layer is locked
         return ("Achievements")
@@ -169,13 +157,13 @@ addLayer("a", {
         11: {
             name: "The first step",
             done() {return player.p.points.gte(1)},
-            tooltip: "Get a 1 tra",
+            tooltip: "Get a 1 Tra<br>Reward: base EXP increases according to ACP",
             onComplete() {player[this.layer].points = player[this.layer].points.add(1)}
         },
         12: {
-            name: "The truth first step",
-            done() {return player.l.points.gte(1)},
-            tooltip: "Reach Level 1",
+            name: "TThe long training begins",
+            done() {return hasUpgrade("p",23)},
+            tooltip: "Purchase TRA Upgrade 13<br>Reward: Unlock LevelUp",
             onComplete() {player[this.layer].points = player[this.layer].points.add(1)}
         },
     },
